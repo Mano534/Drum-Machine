014f3353d70f212b1a7a6eaa7c3428b15d27502c
monmonmon175@yahoo.com <monmonmon175@yahoo.com>
2020-07-17 20:51:50 +0200

    Updates

---
+++ precache-manifest.8de01b629034263762cb36e2fc8bce61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35dce2bfebd842f55aac8360441e2d0",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "a72ba72792fb99a4f6c5",
+    "url": "/Drum-Machine/static/css/main.a2fea2af.chunk.css"
+  },
+  {
+    "revision": "d6f0319ae71494d37fdb",
+    "url": "/Drum-Machine/static/js/2.3c1b4878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drum-Machine/static/js/2.3c1b4878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72ba72792fb99a4f6c5",
+    "url": "/Drum-Machine/static/js/main.73dbaced.chunk.js"
+  },
+  {
+    "revision": "c6dd1430e2afb8c8d750",
+    "url": "/Drum-Machine/static/js/runtime-main.d322cb89.js"
+  }
+]);
